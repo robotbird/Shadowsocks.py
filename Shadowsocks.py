f725b4883712f4caf919851bf72bd8a221d51737
robotbird <robotbird@qq.com>
2016-05-23 10:33:45 +0800

    Update Shadowsocks.py
    获取所有节点密码

--- Shadowsocks.py
+++ Shadowsocks.py
@@ -9,14 +9,20 @@ con =  res.read().decode("utf-8")
 pattern = re.compile('<section id="free">(.*?)</section>',re.S)
 result  = re.search(pattern,con)
 
+
+
 items = re.findall("<h4>(.*?)</h4>",result.group(0))
-pwd = items[2][4:12]
+pwdus = items[2][4:12]
+pwdhk = items[8][4:12]
+pwdjp = items[14][4:12]
 print 'init shadowsocks password'
-print pwd
+print pwdus,pwdhk,pwdjp
 
 with open('gui-config.json','r') as ff:
 	d = json.load(ff)
-	d["configs"][0]["password"]=pwd
+	d["configs"][0]["password"]=pwdjp
+	d["configs"][1]["password"]=pwdus
+	d["configs"][2]["password"]=pwdhk
 
 with open('gui-config.json','w') as f:
 	json.dump(d,f)
